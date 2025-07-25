9fa28897fd7add564e6b96c6221900dd571b84e3
Gabriel Kloper <gabrielkloper@gmail.com>
2025-07-25 11:38:07 -0300

    recebendo token do rn

--- erro.php
+++ erro.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Oportunidades - Desenvolve Cultura</title>
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="./css/style.css">
 </head>
-<body>
-    <h1>Sem permissão para esta páginas</h1>
+ <nav class="navbar navbar-light fixed-top bg-primary text-white px-3">
+    <div class="d-flex align-items-center w-100 justify-content-between">
+      <button class="btn btn-primary border-0 p-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+        <i class="bi bi-list fs-3"></i>
+      </button>
+      <img src="src/logo.svg" alt="Logo" class="img-fluid" style="height: 40px;">
+    </div>
+  </nav>
+<body class="bg-light">
+  <main class="container-fluid py-3">
+    <h1 class="m-3 p-3 text-center fw-bold"></h1>
+    <div class="alert alert-danger" role="alert">
+      Esta página só pode ser acessada pelo aplicativo Desenvolve Cultura.
+    </div>
+    <div class="text-center mb-3"><img src="src/logo.svg" alt="Logo" class="img-fluid" style="height: 500px;"></div>
     
-    <script src="./bootstrap/js/bootstrap.min.js"></script>
+  </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+

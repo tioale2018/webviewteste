0e00995bde61c92a9eff9e980d11231b0068c40d
Gabriel Kloper <gabrielkloper@gmail.com>
2025-07-29 14:42:43 -0300

    vinculando novamente token que pediu para esquecer

--- notifications/salvar-token.php
+++ notifications/salvar-token.php
@@ -11,11 +11,26 @@ $token = $data['token'] ?? null;
 
 file_put_contents('log.txt', json_encode($data));
 
+$salvarToken = salvarToken($cpf, $token);
+
 if ($cpf && $token) {
     try {
-        $stmt = $connPDO->prepare("INSERT IGNORE INTO tokens (cpf, token) VALUES (?, ?)");
-        $stmt->execute([$cpf, $token]);
-        echo json_encode(['success' => true, 'message' => 'Token vinculado ao CPF com sucesso.']);
+         $stmt = $connPDO->prepare("SELECT * FROM tokens WHERE cpf = :cpf AND token = :token AND ativo = 0");
+            $stmt->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+            $stmt->bindParam(':token', $token, PDO::PARAM_STR);
+            $stmt->execute([$cpf, $token]);
+            if ($stmt->rowCount() > 0) {
+                // Se o token estiver vinculado ao CPF, atualiza o status para ativo
+                $stmt = $connPDO->prepare("UPDATE tokens SET ativo = 1 WHERE cpf = :cpf AND token = :token");
+                $stmt->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+                $stmt->bindParam(':token', $token, PDO::PARAM_STR);
+                $stmt->execute([$cpf, $token]);
+                return ['success' => true, 'message' => 'Token já vinculado ao CPF.'];
+            } else {
+            $stmt = $connPDO->prepare("INSERT IGNORE INTO tokens (cpf, token) VALUES (?, ?)");
+            $stmt->execute([$cpf, $token]);
+            return ['success' => true, 'message' => 'Token vinculado ao CPF com sucesso.'];
+            }
     } catch (PDOException $e) {
         http_response_code(500);
         echo json_encode([

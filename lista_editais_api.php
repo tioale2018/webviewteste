51ffc3c07387cd4150024fb379b38027a645f2bb
Gabriel Kloper <gabrielkloper@gmail.com>
2025-09-26 10:06:37 -0300

    atualizando lista projetos e lista editais

--- lista_editais_api.php
+++ lista_editais_api.php
@@ -77,8 +77,8 @@ $token = generate_jwt($payload, $secret);
           <div class="d-flex w-100 justify-content-between">
             <small class="text-muted">${dataCriacaoStr} - ${dataFechamentoStr}</small>
           </div>
-          <div class="mt-2 text-end">
-            <a href="info_edital_api.php?id=${edital.id}" class="btn btn-sm btn-primary w-100">Mais detalhes</a>
+          <div class="mt-2 ">
+            <a href="info_edital_api.php?id=${edital.id}" class="btn btn-sm btn-primary float-end">Mais detalhes</a>
           </div>
         </div>`;
       });

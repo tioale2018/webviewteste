ca42399778fd5447b63b85a07e6263179c0d9683
Gabriel Kloper <gabrielkloper@gmail.com>
2025-10-14 10:47:49 -0300

    mexendo salvar sesssao

--- salvar_sessao.php
+++ salvar_sessao.php
@@ -13,12 +13,12 @@ if ($data && isset($data['user'])) {
     $_SESSION['id_user'] = $data['user']['id_user'];
     $_SESSION['tipo_doc'] = $data['user']['tipo_doc'];
     // $_SESSION['token'] = $token;
-    $_SESSION['token'] = $data['token'];
+    $_SESSION['token'] = $data['user']['token'];
 
     // Salva o token do dispositivo
-    salvarToken($data['user']['cpf'], $token);
+    salvarToken($data['user']['cpf'], $data['user']['token']);
     
-    echo json_encode(['success' => true, 'token' => $token]);
+    echo json_encode(['success' => true, 'token' => $data['user']['token']]);
 } else {
     http_response_code(400);
     echo json_encode(['success' => false, 'message' => 'Dados inválidos']);

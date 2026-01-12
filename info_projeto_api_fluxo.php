4092d675f4501e2bfc2050cbd87ce3230c7d7830
Gabriel Kloper <84879239+gabrielkloper@users.noreply.github.com>
2026-01-12 18:03:33 -0300

    corrigindo novos info_projeto

--- info_projeto_api_fluxo.php
+++ info_projeto_api_fluxo.php
@@ -118,7 +118,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="./js/jquery-3.7.1.min.js"></script>
   <script>
-    const jwtToken = '<?= $token ?>';
+    // jwtToken is already defined globally in navbar.php
     const projectId = '<?= $_GET["id"] ?? "" ?>';
 
     function safe(v) {
